12e0d5cc5a48e6d7840ce619bd4d5cefc3bd8f8a
Janielle Oliveira <janielleoliver.brito@gmail.com>
2023-11-15 04:13:50 -0300

    Documentação

--- src/SistemaLogin.java
+++ src/SistemaLogin.java
@@ -1,32 +1,43 @@
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Um simples Sistema de Login em Java que incorpora operações CRUD.
+ * Permite cadastrar, autenticar, listar, atualizar e excluir usuários.
+ * Os dados dos usuários são armazenados em um array bidimensional e persistidos em um arquivo de texto (usuarios.txt).
+ */
+
 public class SistemaLogin {
 
+    // Array bidimensional para armazenar os dados dos usuários (nome, nome de usuário, senha)
     static String[][] usuarios = new String[10][3];
 
+    /**
+     * Função principal que inicia o Sistema de Login e interage com o usuário.
+     * Permite escolher entre diferentes operações CRUD através de um menu interativo.
+     */
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        carregarDados();
+        Scanner sc = new Scanner(System.in); // Inicializa um Scanner para receber entrada do usuário a partir do console
+        carregarDados();// Carregar dados a partir do arquivo "usuarios.txt" ao iniciar
 
         int opcao;
-        do {
-            System.out.println("**** SISTEMA DE LOGIN ****");
+        do { // Inicia um loop que mantém o programa em execução
+            System.out.println("**** SISTEMA DE LOGIN ****"); // Exibe o menu de opções
             System.out.println("1. Cadastrar Usuário");
             System.out.println("2. Autenticar Usuário");
             System.out.println("3. Listar Usuários");
             System.out.println("4. Atualizar Dados");
             System.out.println("5. Excluir Usuário");
             System.out.println("6. Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.print("Escolha uma opção: ");// Solicita ao usuário uma escolha
 
-            opcao = sc.nextInt();
+            opcao = sc.nextInt();// Lê a escolha do usuário
 
-            switch (opcao) {
+            switch (opcao) {  // Realiza a operação correspondente para lidar com a escolha do usuário
                 case 1:
                     cadastrarUsuario();
                     break;
@@ -43,19 +54,26 @@ public class SistemaLogin {
                     excluirUsuario();
                     break;
                 case 6:
-                    salvarDados();
+                    salvarDados(); // Salva os dados dos usuários no arquivo ao sair
                     System.out.println("Obrigado por usar o Sistema de Login. Até a próxima!");
                     break;
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("Opção inválida. Tente novamente."); // Trata escolhas inválidas
             }
-        } while (opcao != 6);
+        } while (opcao != 6); // Continua o loop enquanto o usuário não escolher a opção 6 (sair)
+
+        sc.close(); // Fecha o Scanner para evitar vazamentos de recursos
     }
 
+    /**
+     * Cadastra um novo usuário no sistema.
+     * Solicita informações como nome completo, nome de usuário e senha.
+     * Os dados do usuário são armazenados no array de usuários.
+     */
     private static void cadastrarUsuario() {
         Scanner sc = new Scanner(System.in);
         System.out.println("---------------------------------------------");
-        System.out.println("        *** CADASTRO DE USUÁRIO ***         ");
+        System.out.println("        *** CADASTRO DE USUARIO ***         ");
         System.out.print("Nome completo: ");
         String nome = sc.nextLine();
         System.out.print("Nome de usuário: ");
@@ -76,6 +94,11 @@ public class SistemaLogin {
         System.out.println("Limite de usuários atingido. Não foi possível cadastrar.");
     }
 
+    /**
+     * Autentica um usuário no sistema.
+     * Solicita nome de usuário e senha e verifica se correspondem a um usuário existente.
+     * Exibe mensagem de autenticação bem-sucedida ou falha.
+     */
     private static void autenticarUsuario() {
         System.out.println("---------------------------------------------");
         System.out.println("**** AUTENTICAR ****");
@@ -95,9 +118,13 @@ public class SistemaLogin {
         System.out.println("Credenciais inválidas. Autenticação falhou.");
     }
 
+    /**
+     * Lista os usuários cadastrados no sistema.
+     * Exibe o nome de cada usuário na saída padrão.
+     */
     private static void listarUsuarios() {
         System.out.println("---------------------------------------------");
-        System.out.println("LISTA DE USUÁRIOS: ");
+        System.out.println("LISTA DE USUARIOS: ");
         for (int i = 0; i < usuarios.length; i++) {
             if (usuarios[i][0] != null) {
                 System.out.println("Usuário: " + usuarios[i][0]);
@@ -106,6 +133,10 @@ public class SistemaLogin {
         System.out.println("---------------------------------------------");
     }
 
+    /**
+     * Atualiza a senha de um usuário existente no sistema.
+     * Solicita o nome de usuário, verifica a existência e permite a atualização da senha.
+     */
     private static void atualizarSenha() {
         Scanner sc = new Scanner(System.in);
         System.out.println("---------------------------------------------");
@@ -126,10 +157,14 @@ public class SistemaLogin {
         System.out.println("Usuário não encontrado. Atualização falhou.");
     }
 
+    /**
+     * Exclui um usuário do sistema.
+     * Solicita o nome de usuário, verifica a existência e remove os dados do usuário.
+     */
     private static void excluirUsuario() {
         Scanner sc = new Scanner(System.in);
         System.out.println("---------------------------------------------");
-        System.out.println("**** DELETAR USUÁRIO ****");
+        System.out.println("**** DELETAR USUARIO ****");
         System.out.print("Digite o nome de usuário para excluir: ");
         String nomeUser = sc.nextLine();
         for (int i = 0; i < usuarios.length; i++) {
@@ -145,13 +180,17 @@ public class SistemaLogin {
         System.out.println("Usuário não encontrado. Exclusão falhou.");
     }
 
+    /**
+     * Carrega os dados dos usuários de um arquivo de texto.
+     * Os dados são armazenados no array de usuários.
+     */
     private static void carregarDados() {
         try (BufferedReader dados = new BufferedReader(new FileReader("usuarios.txt"))) {
             String linha;
             int i = 0;
             while ((linha = dados.readLine()) != null) {
                 String[] partes = linha.split(", ");
-                usuarios[i][0] = partes[0].trim();
+                usuarios[i][0] = partes[0].trim(); // trim(); Remover espaços em branco em excesso
                 usuarios[i][1] = partes[1].trim();
                 usuarios[i][2] = partes[2].trim();
                 i++;
@@ -161,6 +200,10 @@ public class SistemaLogin {
         }
     }
 
+    /**
+     * Salva os dados dos usuários em um arquivo de texto.
+     * Apenas os usuários não nulos são salvos.
+     */
     private static void salvarDados() {
         try (FileWriter arquivo = new FileWriter("usuarios.txt")) {
             for (int i = 0; i < usuarios.length; i++) {
